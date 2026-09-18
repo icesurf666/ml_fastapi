cad0c68611aaa9b4ed6d8336d048a3210b938f68
Pavel Kazantsev <icesurf@yandex.ru>
2026-09-19 02:41:08 +0700

    feat: add basic fast api app

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from schemas import FeatureVectorChurn
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"message": "ml churn service is running"}
+
+
+@app.post("/predict")
+def predict(features: FeatureVectorChurn):
+    return features
